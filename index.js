578f1beacf4d70e1ff754403100901f444e1bdb0
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-03-11 15:45:16 -0500

    reverse a string with built-in methods

--- index.js
+++ index.js
@@ -63,3 +63,12 @@ function concatArray(what) {
    };
 
    removeChar("Anthony");
+
+
+   function solution(str){
+     let splitString = str.split("");
+     let reverseArray = splitString.reverse();
+     let joinArray = reverseArray.join("");
+     return joinArray;
+  
+  }
