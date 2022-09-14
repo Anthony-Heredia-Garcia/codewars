ce82784dacf45fd55c90c55f518671f61572fab7
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-09-14 16:43:57 -0400

    learning sort() methods

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function concatArray(what) {
   let newArray = [].concat.apply([], what);
   return newArray;
 }
-const bigArray = [[1, 2, 3], [4, 5], [6], ["stuff"]];
+const bigArray = [[1, 2, 3], [4, 5], [6], ['stuff']];
 const biggerArray = [[7], [8], [9, 10, 11, 12]];
 
 console.log(concatArray(biggerArray));
@@ -21,8 +21,8 @@ function fun(aParam, bParam, cParam, dParam) {
   console.log(cParam); // 'c'
   console.log(dParam); // 'd'
 }
-const arr = ["b", "c"];
-fun("a", ...arr);
+const arr = ['b', 'c'];
+fun('a', ...arr);
 
 const multArray = [1, 2, 3, 4, 5];
 function multiply(stoof) {
@@ -54,7 +54,7 @@ function removeChar(str) {
   console.log(str);
 }
 
-removeChar("Anthony");
+removeChar('Anthony');
 
 //  function solution(str){
 //    let splitString = str.split("");
@@ -65,10 +65,10 @@ removeChar("Anthony");
 // }
 
 function solution(str) {
-  return str.split("").reverse().join("");
+  return str.split('').reverse().join('');
 }
 
-console.log(solution("Hello"));
+console.log(solution('Hello'));
 
 const numbers = [5, 6, 2, 3, 7];
 
@@ -78,9 +78,9 @@ console.log(max);
 
 function boolToWord(bool) {
   if (bool === true) {
-    return "Yes";
+    return 'Yes';
   } else if (bool === false) {
-    return "No";
+    return 'No';
   }
 }
 
@@ -93,7 +93,7 @@ function boolToWord(bool) {
 // }
 
 function noSpace(x) {
-  x.replace(/\s/g, "");
+  x.replace(/\s/g, '');
   return x;
 }
 
@@ -104,20 +104,20 @@ function solution(str, ending) {
 
 myArray = [1, 2, 2];
 
-function squareSum(numbers){
- for (i = 0; i < numbers.length; i++) {
-   console.log(numbers.value);
- }
+function squareSum(numbers) {
+  for (i = 0; i < numbers.length; i++) {
+    console.log(numbers.value);
+  }
 }
 
 squareSum(myArray);
 
 //Find the Needle in the Haystack
-let pile = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"];
+let pile = ['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'];
 
 function findNeedle(haystack) {
   for (item of haystack) {
-    if (item == "needle"){
+    if (item == 'needle') {
       return 'found the needle at position ' + haystack.indexOf(item);
     }
   }
@@ -128,7 +128,7 @@ console.log(findNeedle(pile));
 //Rental Car Problem
 //$40 per day, $50 off from total if more than 7 days, $20 off if more than 3.
 function rentalCarCost(d) {
-  if (d >= 7){
+  if (d >= 7) {
     return d * 40 - 50;
   } else if (d >= 3) {
     return d * 40 - 20;
@@ -141,15 +141,61 @@ console.log(rentalCarCost(5));
 
 //Testing the .find() and .findIndex() methods
 
-const cats = [{name: 'Squeak'}, {name: 'Mew'}, {name: 'Pumpkin'}, {name: 'Loaf'}]
+const cats = [
+  { name: 'Squeak' },
+  { name: 'Mew' },
+  { name: 'Pumpkin' },
+  { name: 'Loaf' },
+];
+const moreCats = ['Tom', 'Garfield', 'Sylvester', 'Felix'];
 
 const pumpkin = cats.find((user, idx, users) => {
   return user.name === 'Pumpkin';
-})
+});
 
 const loaf = cats.findIndex((user, idx, users) => {
   return user.name === 'Loaf';
-})
+});
 
 console.log(pumpkin);
 console.log(loaf);
+console.log(moreCats.includes('Felix'));
+
+//For Each Loops using Prices
+
+const prices = [0.99, 12.95, 8.59];
+const randomNumbers = [32, 71, 0, -4, 19, 1352];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+// for (const price of prices){
+//   taxAdjustedPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+  const priceObject = { index: idx, taxAdjPrice: price * (1 + tax) };
+  taxAdjustedPrices.push(priceObject);
+});
+
+console.log(taxAdjustedPrices);
+
+// const sortedPrices = randomNumbers.sort((a, b) => {
+//   if (a > b) {
+//     return 1;
+//   } else if (a === b) {
+//     return 0;
+//   } else {
+//     return -1;
+//   }
+// });
+
+const sortedNumbers = randomNumbers.sort((a, b) => {
+  return a - b;
+});
+
+// const sortedNumbers = randomNumbers.sort();
+
+console.log(sortedNumbers);
+
+// console.log(sortedPrices);
+// console.log(prices[0]<prices[1]);
